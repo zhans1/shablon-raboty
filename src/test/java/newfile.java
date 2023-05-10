262c2c961938f2aa1dcb4a5c44d08cbff9609b57
zhans1 <zhansultan18@gmail.com>
2023-05-10 10:46:33 +0600

    upgraded test folder

--- src/test/java/newfile.java
+++ src/test/java/newfile.java
@@ -1,3 +1,15 @@
-public class newfile {
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
+public class newfile {
+    @BeforeAll
+    static void beforeALL(){
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void FirstTest(){
+
+    }
 }
